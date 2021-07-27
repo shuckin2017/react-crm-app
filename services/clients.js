856995d0e0097fbb6bf757c0a965f0aba4e78214
Maxim Schuckin <shuckin@mail.ru>
2021-07-27 19:27:22 +0300

    initl

--- services/clients.js
+++ services/clients.js
@@ -5,6 +5,7 @@ const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 })
 
+
 module.exports = async function getClients() {
   const database_id = process.env.NOTION_DATEBASE_ID_CLIENTS;
   const payload = {
